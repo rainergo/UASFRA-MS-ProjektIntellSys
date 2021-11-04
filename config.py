459492b9d64b5e955d2256e85d2573d591c5775f
uasfra <gogel@stud.fra-uas.de>
2021-11-04 09:41:55 +0100

    Initial

---
+++ config.py
@@ -0,0 +1,3 @@
+siemens_pdf = "/home/uasfra/uasfraProjektIntell/Sample_Reports/Siemens Energy-sustainability-report-2020.pdf"
+allianz_jpg = "/home/uasfra/uasfraProjektIntell/Sample_Reports/Allianz_Group_Sustainability_Report_2020-web-page-106.jpg"
+allianz_pdf = "/home/uasfra/uasfraProjektIntell/Sample_Reports/Allianz_Group_Sustainability_Report_2020-web.pdf"
