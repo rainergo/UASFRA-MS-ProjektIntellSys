8fcd3532d60334e92286286140dd6193aee8baf4
rainergo <rainer.gogel@web.de>
2021-11-10 22:11:49 +0100

    10.11.2021

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-siemens_pdf = "/home/uasfra/uasfraProjektIntell/Sample_Reports/Siemens Energy-sustainability-report-2020.pdf"
-allianz_jpg = "/home/uasfra/uasfraProjektIntell/Sample_Reports/Allianz_Group_Sustainability_Report_2020-web-page-106.jpg"
-allianz_pdf = "/home/uasfra/uasfraProjektIntell/Sample_Reports/Allianz_Group_Sustainability_Report_2020-web.pdf"
+siemens_pdf = '../1_Reports/Sample_Reports/Siemens Energy-sustainability-report-2020.pdf'
+allianz_jpg = '../1_Reports/Sample_Reports/Allianz_Group_Sustainability_Report_2020-web-page-106.jpg'
+allianz_pdf = '../1_Reports/Sample_Reports/Allianz_Group_Sustainability_Report_2020-web.pdf'
+siemens_page_23 = '../1_Reports/Sample_Reports/SiemensPage23.pdf'
+siemens_page_27 = '../1_Reports/Sample_Reports/SiemensPage27.pdf'
