86a0063d3c07e00c3bd9f6349640dde138298ca4
rainergo <rainer.gogel@web.de>
2021-11-13 13:13:15 +0100

    13.11.2021

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ allianz_jpg = '../1_Reports/Sample_Reports/Allianz_Group_Sustainability_Report_2
 allianz_pdf = '../1_Reports/Sample_Reports/Allianz_Group_Sustainability_Report_2020-web.pdf'
 siemens_page_23 = '../1_Reports/Sample_Reports/SiemensPage23.pdf'
 siemens_page_27 = '../1_Reports/Sample_Reports/SiemensPage27.pdf'
+allianz_page_106 = '../1_Reports/Sample_Reports/Allianz_Group_Sustainability_Report_2020-web-page-106.pdf'
